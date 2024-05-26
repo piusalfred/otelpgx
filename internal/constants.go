53ac0c690e7e6046523852e6ee32c50b5ad911d9
Pius Alfred <me.pius1102@gmail.com>
2024-05-26 13:56:00 +0200

    update libs

--- internal/constants.go
+++ internal/constants.go
@@ -1,10 +1,6 @@
 package internal
 
 const (
-	// TracerName is the name of the tracer. This will be used as an attribute
-	// on each span.
-	TracerName = "github.com/exaring/otelpgx"
-
 	// MeterName is the name of the metric meter.
 	MeterName = "github.com/exaring/otelpgx"
 
